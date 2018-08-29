9221adf77c2e8a328df93cf38bcba1a3d194f0ac
jwuestef <jwuestef@gmail.com>
2018-08-29 08:25:43 -0400

    final status

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace DbFirst
 {
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            var dbContext = new PlutoDbContext();
+
+            //dbContext.GetAuthorCourses();
+
+            var courses = dbContext.GetCourses();
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
+
+
+
+            var course = new Course();
+            course.Level = CourseLevel.Beginner; // 1
+
         }
     }
 }
